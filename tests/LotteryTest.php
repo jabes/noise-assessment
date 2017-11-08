c7112573994f5c79e32ef4094b0503e411b70591
Justin Bull <bull.justin@gmail.com>
2017-11-08 08:41:25 -0800

    Add more unit tests

--- tests/LotteryTest.php
+++ tests/LotteryTest.php
@@ -6,20 +6,33 @@ use Justin\NoiseAssessment\Lottery;
 
 final class LotteryTest extends TestCase
 {
+
   public function testValidNumbers(): void
   {
-    $numbers = [
-      '569815571556',
-      '4938532894754',
-      '1234567',
-      '472844278465445',
-    ];
     $this->assertEquals(
-      Lottery::getValidNumbers($numbers),
+      Lottery::getValidNumbers([
+        '569815571556',
+        '4938532894754',
+        '1234567',
+      ]),
       [
+        '569815571556' => '56 9 8 15 57 15 56',
         '4938532894754' => '49 38 53 28 9 47 54',
         '1234567' => '1 2 3 4 5 6 7',
       ]
     );
   }
+
+  public function testInvalidNumbers(): void
+  {
+    $this->assertEquals(
+      Lottery::getValidNumbers([
+        '472844278465445',
+      ]),
+      [
+        // empty
+      ]
+    );
+  }
+
 }
